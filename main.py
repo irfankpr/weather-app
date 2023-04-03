4fa37456d2c24ee4f440e797f941cdec0701230f
irfankpr <irfanmuhammedkpr@gmail.com>
2023-04-03 07:52:48 +0530

    exception handeled

--- main.py
+++ main.py
@@ -20,27 +20,39 @@ OW_key=headers["authorization"]
 def find_by_name(place):
    url = f"https://api.openweathermap.org/data/2.5/weather?q={place}&appid={OW_key}"
    weather = rq.post(url).json()
-   coord=weather["coord"]
-   more=weather["weather"][0]
-   main=weather["main"]
-   zone=weather["name"]
-   st.session_state["weather_state"]=True
-   return coord,more,main,zone
+   try:
+      coord=weather["coord"]
+      more=weather["weather"][0]
+      main=weather["main"]
+      zone=weather["name"]
+      st.session_state["weather_state"]=True
+      return coord,more,main,zone
+   except KeyError:
+      st.error("No place found, please verify the name !")
+      st.stop()
+   
+   
 
 def find_by_loc():
    myloc=geo.ip("me")
-   weather=rq.post(f"https://api.openweathermap.org/data/2.5/weather?lat={myloc.latlng[0]}&lon={myloc.latlng[1]}&appid={OW_key}").json()
-   coord=weather["coord"]
-   more=weather["weather"][0]
-   main=weather["main"]
-   zone=weather["name"]
-   st.session_state["weather_state"]=True
-   return coord,more,main,zone
+   try:   
+      weather=rq.post(f"https://api.openweathermap.org/data/2.5/weather?lat={myloc.latlng[0]}&lon={myloc.latlng[1]}&appid={OW_key}").json()
+      coord=weather["coord"]
+      more=weather["weather"][0]
+      main=weather["main"]
+      zone=weather["name"]
+      st.session_state["weather_state"]=True
+      return coord,more,main,zone
 
+   except KeyError:
+      st.error("No place found, please verify the name !")
+      st.stop()
+      
+   
 
 def main():
    st.markdown("<h1 style='text-align: center; color: CadetBlue;'>Weather App</h1>", unsafe_allow_html=True)
-   place = st.text_input("Enter the Place").lower()
+   place = st.text_input("Enter the Place").lower().strip()
    c1,c2,c3=st.columns([.5,3,1])
    show= c1.button("Show")
    loc = c3.button("my location")
@@ -54,18 +66,24 @@ def main():
       coord,more,main,zone=find_by_loc()
 
    if st.session_state["weather_state"]:
-      c1,c2=st.columns(2)
-      unit =" °C"
-      t=str(round(main["temp"]-273.15,4))+unit
-      avg=round((main["temp_min"]+main["temp_max"])/2-273.15,4)
+      try:
+         c1,c2=st.columns(2)
+         unit =" °C"
+         t=str(round(main["temp"]-273.15,4))+unit
+         avg=round((main["temp_min"]+main["temp_max"])/2-273.15,4)
 
-      c1.metric(label=zone,value=t)
-      c1.write(f"latlong - {coord['lon']} : {coord['lat']}")
-      c1.header(f"feels like {round(main['feels_like']-273.15,4)}")
+         c1.metric(label=zone,value=t)
+         c1.write(f"latlong - {coord['lon']} : {coord['lat']}")
+         c1.header(f"feels like {round(main['feels_like']-273.15,4)}")
 
-      c2.image(f"https://openweathermap.org/img/wn/{ more['icon'] }@2x.png")
-      c2.write(more["description"])
-      c2.write(f"humidity: {main['humidity']}")
+         c2.image(f"https://openweathermap.org/img/wn/{ more['icon'] }@2x.png")
+         c2.write(more["description"])
+         c2.write(f"humidity: {main['humidity']}")
+      except KeyError:
+         st.error("No place found, please verify the name !")
+      except:
+         st.warning("something went wrong please try again")
+         st.stop()
    else:
       st.stop()
 
